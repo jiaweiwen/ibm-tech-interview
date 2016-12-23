046ce80e5256d30b240dcb5d3b972f1cec991787
jiaweiwen <jwen01@email.wm.edu>
2016-12-23 12:21:48 -0500

    modified:   README.md 	modified:   find_longest_path_multi.py 	modified:   find_longest_path_single.py 	deleted:    find_longest_path_test.py

--- find_longest_path_multi.py
+++ find_longest_path_multi.py
@@ -4,15 +4,15 @@ top left conner of the 2-d grid in a multi-threading manner.
 To run this script in command: $python find_longest_path_multi.py 
 Sample input files: input_test.txt
 """
+
 import datetime
 import threading
 from threading import Thread
 
-thread_lock = threading.Lock()
-thread_num = 32
-threads, final_seq = [], []
+thread_num = 4
+threads, max_step, thread_id = [], [0] * thread_num * 3, 0
 
-def LongestSequence(target_grid, row_idx, col_idx, cand_seq):
+def LongestSequence(target_grid, row_idx, col_idx, num_step):
     """
     This function finds the longest sequence on four different directions with
     the help of threading.
@@ -21,73 +21,46 @@ def LongestSequence(target_grid, row_idx, col_idx, cand_seq):
         target_grid: the original input 2-d grid.
         row_idx: the row index of the current position.
         col_idx: the col index of the current position.
-        cand_seq: the list of candidate sequence for the current search.
+        num_step: the number of steps for the current search.
 
     Output:
         None
     """
-    global thread_lock, thread_num, threads, final_seq
 
-    # If the target_grid is an empty list or not.
+    global thread_num, threads, thread_id, max_step
+
+    # If the input is empty, return.
     if len(target_grid) == 0 or len(target_grid[0]) == 0:
-        return []
+        return
 
+    # Calculate the bounds for rows and columns.
     row_len, col_len = len(target_grid), len(target_grid[0])
 
-    # Append the current number to the candidate sequence.
-    cand_seq.append(target_grid[row_idx][col_idx])
-
     # Determine if we have found a new consecutive path with longer length. 
-    thread_lock.acquire()
-    if len(cand_seq) > len(final_seq): 
-        del final_seq[:]
-        for cand in cand_seq:
-            final_seq.append(cand)
-    thread_lock.release()
-    
-    # Search on the up direction.
-    if row_idx > 0 and (target_grid[row_idx - 1][col_idx] == 
-                        target_grid[row_idx][col_idx] + 1):
-        if threading.activeCount() < thread_num:
-            tmp_thread = Thread(target=LongestSequence, 
-                args=(target_grid, row_idx - 1, col_idx, list(cand_seq)))
-            tmp_thread.start()
-            threads.append(tmp_thread)
-        else:
-            LongestSequence(target_grid, row_idx - 1, col_idx, list(cand_seq)) 
-        
-    # Search on the down direction.
-    if row_idx < row_len - 1 and (target_grid[row_idx + 1][col_idx] == 
-                                  target_grid[row_idx][col_idx] + 1):
-        if threading.activeCount() < thread_num:
-            tmp_thread = Thread(target=LongestSequence, 
-                args=(target_grid, row_idx + 1, col_idx, list(cand_seq)))
-            tmp_thread.start()
-            threads.append(tmp_thread)
-        else:
-            LongestSequence(target_grid, row_idx + 1, col_idx, list(cand_seq)) 
+    if num_step > max_step[thread_id - 1]: 
+        max_step[thread_id - 1] = num_step
 
-    # Search on the left direction.
-    if col_idx > 0 and (target_grid[row_idx][col_idx - 1] == 
-                        target_grid[row_idx][col_idx] + 1):
-        if threading.activeCount() < thread_num:
-            tmp_thread = Thread(target=LongestSequence, 
-                args=(target_grid, row_idx, col_idx - 1, list(cand_seq)))
-            tmp_thread.start()
-            threads.append(tmp_thread)
-        else:
-            LongestSequence(target_grid, row_idx, col_idx - 1, list(cand_seq)) 
+    # Generate the all the possible position in the next search.
+    row_col_idx_list = [(row_idx - 1, col_idx), (row_idx + 1, col_idx),
+                        (row_idx, col_idx - 1), (row_idx, col_idx + 1)]
 
-    # Search on the right direction.
-    if col_idx < col_len - 1 and (target_grid[row_idx][col_idx + 1] == 
-                                  target_grid[row_idx][col_idx] + 1):
-        if threading.activeCount() < thread_num:
-            tmp_thread = Thread(target=LongestSequence, 
-                args=(target_grid, row_idx, col_idx + 1, list(cand_seq)))
-            tmp_thread.start()
-            threads.append(tmp_thread)
-        else:
-            LongestSequence(target_grid, row_idx, col_idx + 1, list(cand_seq)) 
+    # Determine if we will do the next search for all the possibilities.
+    for (new_row, new_col) in row_col_idx_list:
+        if (new_row >= 0 and new_row < row_len and 
+            new_col >= 0 and new_col < col_len and
+            target_grid[new_row][new_col] == target_grid[row_idx][col_idx] + 1):
+            
+            # Implement multi-threading if the number of threads is less than
+            # the predefined threshold for number of threads.
+            if (threading.activeCount() < thread_num and 
+                thread_id < len(max_step)):
+                thread_id += 1
+                tmp_thread = Thread(target=LongestSequence, 
+                    args=(target_grid, new_row, new_col, num_step + 1))
+                tmp_thread.start()
+                threads.append(tmp_thread)
+            else:
+                LongestSequence(target_grid, new_row, new_col, num_step + 1) 
 
     return
 
@@ -98,16 +71,25 @@ def main():
     with open(input_file, "r") as file:
         target_grid = [[int(x) for x in line.split(',')] for line in file]
 
+    # Record the starting time of the script.
     start = datetime.datetime.now()
 
     # Determine the longest consecutive sequence of the target grid.
-    LongestSequence(target_grid, 0, 0, [])
+    LongestSequence(target_grid, 0, 0, 0)
     
     for t in threads:
         t.join()
 
+    # Record the ending time of the script.
     end = datetime.datetime.now()
 
+    # Record the final sequence starting from the top left conner.
+    if len(target_grid) == 0 or len(target_grid[0]) == 0:
+        final_seq = []
+    else:
+        final_seq = range(target_grid[0][0], 
+                          max(max_step) + target_grid[0][0] + 1)
+    
     print 'The longest consecutive sequence is ', final_seq
     print 'Execution time is %d ms' %int((end-start).total_seconds() * 1000000)
 
--- find_longest_path_single.py
+++ find_longest_path_single.py
@@ -1,12 +1,15 @@
 """This script aims at find the longest consecutive sequence starting from the 
-top left conner of the 2-d grid.
+top left conner of the 2-d grid in a single-threading manner.
 
-To run this script in command: $python find_longest_path.py 
+To run this script in command: $python find_longest_path_single.py 
 Sample input files: input_test.txt
 """
+
 import datetime
 
-def LongestSequence(target_grid, row_idx, col_idx, cand_seq, final_seq):
+max_step = 0
+
+def LongestSequence(target_grid, row_idx, col_idx, num_step):
     """
     This function finds the longest sequence on four different directions.
 
@@ -14,52 +17,38 @@ def LongestSequence(target_grid, row_idx, col_idx, cand_seq, final_seq):
         target_grid: the original input 2-d grid.
         row_idx: the row index of the current position.
         col_idx: the col index of the current position.
-        cand_seq: the list of candidate sequence for the current search.
-        final_seq: the list of candidate sequence with the longest length
-            for the current search.
+        num_step: the number of steps for the current search.
 
     Output:
-        final_seq: the list of candidate sequence with the longest length
-            for the current search.
+        None
     """
 
-    # If the target_grid is an empty list or not.
-    if len(target_grid) == 0 or len(target_grid[0]) == 0:
-        return []
+    global max_step
 
+    # If the input is empty, return.
+    if len(target_grid) == 0 or len(target_grid[0]) == 0:
+        return
+
+    # Calculate the bounds for rows and columns.
     row_len, col_len = len(target_grid), len(target_grid[0])
 
-    # Append the current number to the candidate sequence.
-    cand_seq.append(target_grid[row_idx][col_idx])
+    # Determine if we have found a longer path. 
+    if num_step > max_step: 
+        max_step = num_step
 
-    # Determine if we have found a new consecutive path with longer length.
-    if len(cand_seq) > len(final_seq):
-        del final_seq[:]
-        for cand in cand_seq:
-            final_seq.append(cand)
+    # Generate the all the possible position in the next search.
+    row_col_idx_list = [(row_idx - 1, col_idx), (row_idx + 1, col_idx),
+                        (row_idx, col_idx - 1), (row_idx, col_idx + 1)]
 
-    # Search on the up direction.
-    if row_idx > 0 and (target_grid[row_idx - 1][col_idx] == 
-                        target_grid[row_idx][col_idx] + 1):
-        LongestSequence(target_grid, row_idx - 1, col_idx, cand_seq, final_seq)
-        
-    # Search on the down direction.
-    if row_idx < row_len - 1 and (target_grid[row_idx + 1][col_idx] == 
-                                  target_grid[row_idx][col_idx] + 1):
-        LongestSequence(target_grid, row_idx + 1, col_idx, cand_seq, final_seq)
+    # Determine if we will do the next search for all the possibilities.
+    for (new_row, new_col) in row_col_idx_list:
+        if (new_row >= 0 and new_row < row_len and 
+            new_col >= 0 and new_col < col_len and
+            target_grid[new_row][new_col] == target_grid[row_idx][col_idx] + 1): 
 
-    # Search on the left direction.
-    if col_idx > 0 and (target_grid[row_idx][col_idx - 1] == 
-                        target_grid[row_idx][col_idx] + 1):
-        LongestSequence(target_grid, row_idx, col_idx - 1, cand_seq, final_seq)
+            LongestSequence(target_grid, new_row, new_col, num_step + 1) 
 
-    # Search on the right direction.
-    if col_idx < col_len - 1 and (target_grid[row_idx][col_idx + 1] == 
-                                  target_grid[row_idx][col_idx] + 1):
-        LongestSequence(target_grid, row_idx, col_idx + 1, cand_seq, final_seq)
-    
-    cand_seq.pop(-1)
-    return final_seq
+    return
 
 
 def main():
@@ -68,10 +57,20 @@ def main():
     with open(input_file, "r") as file:
         target_grid = [[int(x) for x in line.split(',')] for line in file]
 
+    # Record the starting time of the script.
     start = datetime.datetime.now()
+    
     # Determine the longest consecutive sequence of the target grid.
-    final_seq = LongestSequence(target_grid, 0, 0, [], [])
+    LongestSequence(target_grid, 0, 0, 0)
+
+    # Record the ending time of the script.
     end = datetime.datetime.now()
+
+    # Record the final sequence starting from the top left conner.
+    if len(target_grid) == 0 or len(target_grid[0]) == 0:
+        final_seq = []
+    else:
+        final_seq = range(target_grid[0][0], max_step + target_grid[0][0] + 1)
 
     print 'The longest consecutive sequence is ', final_seq
     print 'Execution time is %d ms' %int((end-start).total_seconds() * 1000000)
